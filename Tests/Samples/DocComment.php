940aaafe9ae72093e3b420604ba9d99dff68f132
joy2fun <web@css.js.cn>
2015-03-15 18:51:22 +0800

    add sample files.

---
+++ Tests/Samples/DocComment.php
@@ -0,0 +1,25 @@
+<?php
+
+class MissingCommentClass
+{
+
+    /**
+     * emtpy lines
+     */
+
+    public function missingCommnetMethod()
+    {
+    }
+
+    // inline commnet
+    public function nonDocBlockFunction()
+    {
+    }
+
+    /**
+     * It's fine
+     */
+    public function withDocComment()
+    {
+    }
+}
---
+++ Tests/Samples/PrivateMethodUnderscore.php
@@ -0,0 +1,12 @@
+<?php
+
+class PrivateMethodUnderscore
+{
+    private function withoutScore()
+    {
+    }
+
+    private function _withScore()
+    {
+    }
+}
